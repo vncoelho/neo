052beb48fe6e50ee7027017869fb02da7a204383
Vitor <vncoelho@gmail.com>
2018-12-01 06:28:03 -0200

    Moving maxBlock for messages to GetBlocksPayload

--- neo/Network/P2P/Payloads/GetBlocksPayload.cs
+++ neo/Network/P2P/Payloads/GetBlocksPayload.cs
@@ -7,14 +7,16 @@ namespace Neo.Network.P2P.Payloads
     {
         public UInt256[] HashStart;
         public UInt256 HashStop;
+        public int MaxBlocksCount;
 
-        public int Size => HashStart.GetVarSize() + HashStop.Size;
+        public int Size => HashStart.GetVarSize() + HashStop.Size + sizeof(int);
 
-        public static GetBlocksPayload Create(UInt256 hash_start, UInt256 hash_stop = null)
+        public static GetBlocksPayload Create(UInt256 hash_start, int max_block_count = 1, UInt256 hash_stop = null)
         {
             return new GetBlocksPayload
             {
                 HashStart = new[] { hash_start },
+                MaxBlocksCount = max_block_count,
                 HashStop = hash_stop ?? UInt256.Zero
             };
         }
@@ -23,12 +25,14 @@ namespace Neo.Network.P2P.Payloads
         {
             HashStart = reader.ReadSerializableArray<UInt256>(16);
             HashStop = reader.ReadSerializable<UInt256>();
+            MaxBlocksCount = reader.ReadInt32();
         }
 
         void ISerializable.Serialize(BinaryWriter writer)
         {
             writer.Write(HashStart);
             writer.Write(HashStop);
+            writer.Write(MaxBlocksCount);
         }
     }
 }
